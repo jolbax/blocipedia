2b0cef71bca95392fe37a79792dcc789cff73a0d
Jose Luis Barahona <jolubax@icloud.com>
2019-03-25 20:23:47 +0100

    WIP deploy to production

---
+++ src/config/app-config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const path = require("path");
+const bodyParse = require("body-parser");
+const flash = require("express-flash");
+const logger = require("morgan");
+const session = require("express-session");
+const viewsFolder = path.join(__dirname, "..", "views");
+
+module.exports = {
+  init(app, express) {
+    app.set("views", viewsFolder);
+    app.set("view engine", "ejs");
+    app.use(logger("dev"));
+    app.use(bodyParse.urlencoded({extended: true}));
+    app.use(session({
+      secret: process.env.cookieSecret,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 60000 }
+    }));
+    app.use(flash());
+    app.use(express.static(path.join(__dirname, "..", "assets")));
+  }
+};
